80adba850ccd57270726fb2f0c0d068362ad6e6f
Austin Martinez <austinm@cpp.edu>
2021-09-29 17:29:43 -0700

    Added Function: GetTripDays

--- GetsFile.h
+++ GetsFile.h
@@ -1,1 +1,18 @@
 #include <stdio.h>
+
+int getTripDays()
+{
+    int numOfDays;
+   
+    printf("Please input the number of days you spent on the trip.");
+
+    scanf("%d", &numOfDays);
+
+    while (numOfDays <= 0)
+    {
+        printf("Sorry, that is not a valid input. Please input a value greater than 0.");
+        numOfDays = scanf("%d", &numOfDays);
+    }
+
+    return numOfDays;
+}
