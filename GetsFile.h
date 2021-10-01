378df3f2b681caa11389bf99f46fc2a9351143f7
Austin Martinez <austinm@cpp.edu>
2021-10-01 16:27:27 -0700

    Added GetsFile.c/Added milesDriven()/Fixes

--- GetsFile.h
+++ GetsFile.h
@@ -1,18 +1,8 @@
 #include <stdio.h>
 
-int getTripDays()
-{
-    int numOfDays;
-   
-    printf("Please input the number of days you spent on the trip.");
-
-    scanf("%d", &numOfDays);
-
-    while (numOfDays <= 0)
-    {
-        printf("Sorry, that is not a valid input. Please input a value greater than 0.");
-        numOfDays = scanf("%d", &numOfDays);
-    }
-
-    return numOfDays;
-}
+#ifndef GETSFILE_H
+#define GETSFILE_H
+int getTripDays();
+float milesDriven();
+int departureTime();
+#endif
