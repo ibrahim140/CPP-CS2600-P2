70c1124e2e00f9645c34b6f5a912095fa0617612
SMolletSchool <shmollet@cpp.edu>
2021-10-02 13:41:29 -0700

    Added All-Seeing Ampersands

--- fees.h
+++ fees.h
@@ -3,11 +3,11 @@
 float TaxiFee () {
     float fee;
     printf("\nHow much was paid in taxi fees this day? ");
-    scanf("%f", fee);
+    scanf("%f", &fee);
     while (fee < 0) {
         printf("\nInvalid input! fee should be from 0 to %d (though hopefully not that far).", __INT_MAX__);
         printf("\nHow much was paid in taxi fees this day? ");
-        scanf("%f", fee);
+        scanf("%f", &fee);
     }
     return fee; 
 }
@@ -16,11 +16,11 @@ float TaxiFeeMultiDays (int days) {
     float fee = 0, dayFee;
     for (int i = 0; i < days; i++) {
         printf("\nHow much was paid in taxi fees on day %d? ", i+1);
-        scanf("%f", dayFee);
+        scanf("%f", &dayFee);
         while (dayFee < 0) {
             printf("\nInvalid input! fee should be from 0 to %d (though hopefully not that far).", __INT_MAX__);
             printf("\nHow much was paid in taxi fees on day %d? ", i+1);
-            scanf("%f", dayFee);
+            scanf("%f", &dayFee);
         }
         fee += dayFee;
     }
@@ -30,11 +30,11 @@ float TaxiFeeMultiDays (int days) {
 float HotelFee () {
     float fee;
     printf("\nHow much was paid in hotel fees this day? ");
-    scanf("%f", fee);
+    scanf("%f", &fee);
     while (fee < 0) {
             printf("\nInvalid input! fee should be from 0 to %d (though hopefully not that far).", __INT_MAX__);
             printf("\nHow much was paid in hotel fees this day? ");
-            scanf("%f", fee);
+            scanf("%f", &fee);
     }
     return fee; 
 }
@@ -43,11 +43,11 @@ float HotelFeeMultiDays (int days) {
     float fee = 0, dayFee;
     for (int i = 0; i < days; i++) {
         printf("\nHow much was paid in hotel fees on day %d? ", i+1);
-        scanf("%f", dayFee);
+        scanf("%f", &dayFee);
         while (dayFee < 0) {
             printf("\nInvalid input! fee should be from 0 to %d (though hopefully not that far).", __INT_MAX__);
             printf("\nHow much was paid in hotel fees on day %d? ", i+1);
-            scanf("%f", dayFee);
+            scanf("%f", &dayFee);
         }
         fee += dayFee;
     }
@@ -57,11 +57,11 @@ float HotelFeeMultiDays (int days) {
 float ParkingFee () {
     float fee;
     printf("\nHow much was paid in parking fees this day? ");
-    scanf("%f", fee);
+    scanf("%f", &fee);
     while (fee < 0) {
             printf("\nInvalid input! fee should be from 0 to %d (though hopefully not that far).", __INT_MAX__);
             printf("\nHow much was paid in parking fees this day? ");
-            scanf("%f", fee);
+            scanf("%f", &fee);
     }
     return fee; 
 }
@@ -70,11 +70,11 @@ float ParkingFeeMultiDays (int days) {
     float fee = 0, dayFee;
     for (int i = 0; i < days; i++) {
         printf("\nHow much was paid in parking fees on day %d? ", i+1);
-        scanf("%f", dayFee);
+        scanf("%f", &dayFee);
         while (dayFee < 0) {
             printf("\nInvalid input! fee should be from 0 to %d (though hopefully not that far).", __INT_MAX__);
             printf("\nHow much was paid in parking fees on day %d? ", i+1);
-            scanf("%f", dayFee);
+            scanf("%f", &dayFee);
         }
         fee += dayFee;
     }
@@ -84,11 +84,11 @@ float ParkingFeeMultiDays (int days) {
 float ConferenceFee () {
     float fee;
     printf("\nHow much was paid in conference fees this day? ");
-    scanf("%f", fee);
+    scanf("%f", &fee);
     while (fee < 0) {
             printf("\nInvalid input! fee should be from 0 to %d (though hopefully not that far).", __INT_MAX__);
             printf("\nHow much was paid in conference fees this day? ");
-            scanf("%f", fee);
+            scanf("%f", &fee);
     }
     return fee; 
 }
@@ -97,11 +97,11 @@ float ConferenceFeeMultiDays (int days) {
     float fee = 0, dayFee;
     for (int i = 0; i < days; i++) {
         printf("\nHow much was paid in conference fees on day %d? ", i+1);
-        scanf("%f", dayFee);
+        scanf("%f", &dayFee);
         while (dayFee < 0) {
             printf("\nInvalid input! fee should be from 0 to %d (though hopefully not that far).", __INT_MAX__);
             printf("\nHow much was paid in conference fees on day %d? ", i+1);
-            scanf("%f", dayFee);
+            scanf("%f", &dayFee);
         }
         fee += dayFee;
     }
