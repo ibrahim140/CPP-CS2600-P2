a852296b2a5c24826741127403b9d2e6a20788b1
Mohammed Ibrahim <mohammedi@cpp.edu>
2021-10-02 16:13:40 -0700

    edit mealCosts function to return amount covered

--- fees.h
+++ fees.h
@@ -106,54 +106,44 @@ float ConferenceFeeMultiDays (int days) {
         fee += dayFee;
     }
     return fee;
-
-    //meals function:
-    
-
 }
 
 //meals function
-float mealCosts(int amountOfDays, int arrivalTime, int departureTime, float totalMealCosts)
+float mealCosts(int arrivalTime, int departureTime)
 {
-    int breakfast = 9, lunch = 12, dinner = 16;
-   
-    while(totalMealCosts <= 0)
-    {
-        printf("Please enter a valid amount: ")
-        scanf("%f", &totalMealCosts);
-    }
+    const int BREAKFAST = 9, LUNCH = 12, DINNER = 16;
+
+    int mealCostCovered = 0;
     
     // meals are allowed if departure time is before 7 am, 12 pm, & 6 pm
     // if-else-if structure for meals on first day of trip (departure)
     if(departureTime <= 7)
     {
-        totalMealCosts -= breakfast;
+        mealCostCovered += BREAKFAST;
     }
     else if ((departureTime > 7) && (departureTime <= 12))
     {
-        totalMealCosts -= lunch;
+        mealCostCovered += LUNCH;
     }
     else if ((departureTime > 12) && (departureTime <= 18))
     {
-        totalMealCosts -= dinner;
+        mealCostCovered += DINNER;
     }
     
     // meals are allowed if arrival time is after 8 am, 1 pm, & 7 pm
     // if-else-if structure for meals on last day of trip (arrival)
     if(arrivalTime > 8)
     {
-        totalMealCosts -= breakfast;
+        mealCostCovered += BREAKFAST;
     }
     else if (arrivalTime > 13)
     {
-        totalMealCosts -= lunch;
+        mealCostCovered += LUNCH;
     }
     else if (arrivalTime > 19)
     {
-        totalMealCosts -= dinner;
+        mealCostCovered += DINNER;
     }
 
-    /* return the total meal costs after removing the amount that the company would cover
-        if the value is positive, then you need to reimburrse, if negative, you saved. */
-    return (totalMealCosts -= (amountOfDays*(breakfast + lunch + dinner)));
+    return mealCostCovered;
 }
