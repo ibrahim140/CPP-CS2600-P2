4d054715c8f54ec32328158984bc7325191f7bd9
Mohammed Ibrahim <mohammedi@cpp.edu>
2021-10-03 14:16:27 -0700

    fixed minor grammer issues in print statements

--- finalExpenses.h
+++ finalExpenses.h
@@ -6,22 +6,21 @@ and the finally the amount saved or needed to be reimbursed by the employee.
 #include <stdio.h>
 
 void displayTotalExpenses(float totalExpense){
-    printf("\nThe total expenses incurred by the businessperson were $%.2f.\n", totalExpense);
+    printf("\nThe total expenses incurred by the businessperson is $%.2f.\n", totalExpense);
 }
 
 void displayAllowedExpenses(float allowedExpense){
-    printf("The total allowed expenses for the trip are $%.2f.\n", allowedExpense);
-   
+    printf("The total allowed expenses for the trip is $%.2f.\n", allowedExpense);
 }
 
 void finalTotalExpenses(float totalExpense, float allowedExpense){
     float finalTotal = allowedExpense - totalExpense;
 
     if(finalTotal < 0){
-        printf("The excess expenses that must be reimbursed by the businessperson are $%.2f.\n", (finalTotal*-1));
+        printf("The excess expenses that must be reimbursed by the businessperson is $%.2f.\n", (finalTotal*-1));
     }else if(finalTotal > 0){
-        printf("The amount saved by the businessperson was $%.2f.\n", finalTotal);
+        printf("The amount saved by the businessperson is $%.2f.\n", finalTotal);
     }else{
-        printf("The total expenses were equal to the allowed expenses, no reimbursal needed.\n");
+        printf("The total expenses are equal to the allowed expenses, no reimbursal required.\n");
     }
 }
